d0711066b9d73043ce6faa36f84d18d130ceffc9
desmo13 <mariodesmo13@gmail.com>
2022-09-28 17:03:58 +0100

    Creado boton de borrar y funcional

--- Controllers/PersonajeController.cs
+++ Controllers/PersonajeController.cs
@@ -10,9 +10,9 @@ namespace AUT02_02_GaldonMario_Listas.Controllers
 
             public static List<Personaje> listaPersonaje = new List<Personaje>()
             {
-                new Personaje() { Familia = "Familia1", Id = 1, Name = "Nombre1", NumHijos = 1 },
-                new Personaje() { Familia = "Familia2", Id = 2, Name = "Joselito", NumHijos = 1 },
-                new Personaje() { Familia = "Familia3", Id = 3, Name = "Manolo", NumHijos = 10 }
+                new Personaje() { Familia = "Familia1", Id = 0, Name = "Nombre1", NumHijos = 1 },
+                new Personaje() { Familia = "Familia2", Id = 1, Name = "Joselito", NumHijos = 1 },
+                new Personaje() { Familia = "Familia3", Id = 2, Name = "Manolo", NumHijos = 10 }
             };
           
         
@@ -43,6 +43,27 @@ namespace AUT02_02_GaldonMario_Listas.Controllers
             {
                 return View();
             }
+        }
+
+        /*Funcion que es llamada cuando se apreta el boton de llamar de la lista 
+         * Cada boton que se genera en el formulario se genera con un valor que envia diferente que es el ID de la persona 
+         * Luego lo recogemos en esta funcion y en un bucle foreach vamos comparando si encuentra un id en la lista igual al enviado lo borra y sale del bucle con un break
+         * PD:que pestano no vea que e metido un break en un blucle for si no me mata
+         */
+        public IActionResult Delete(int Id)
+        {
+            foreach(var elemento in listaPersonaje)
+            {
+                if (elemento.Id == Id)
+                {
+                    listaPersonaje.Remove(elemento);
+                    break;
+                }
+            }
+
+                
+            
+            return RedirectToAction("Index");
         }
     }
 }
