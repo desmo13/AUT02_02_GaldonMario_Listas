a51ad7e4c06a59a117918d33082dc440e0f67c57
2dawa <2dawa@PC23312>
2022-09-28 08:58:13 +0100

    Agregar archivos de proyecto.

---
+++ Controllers/PersonajeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AUT02_02_GaldonMario_Listas.Models;
+using System.Xml.Linq;
+
+namespace AUT02_02_GaldonMario_Listas.Controllers
+{
+
+    public class PersonajeController : Controller
+    {
+
+            public List<Personaje> listaPersonaje = new List<Personaje>()
+            {
+               
+                new Personaje() { Familia = "Familia2", Id = 2, Name = "Joselito", NumHijos = 1 },
+                new Personaje() { Familia = "Familia3", Id = 3, Name = "Manolo", NumHijos = 10 }
+            };
+          
+        
+
+        public IActionResult Index()
+        {
+            listaPersonaje.Add(new Personaje() { Familia = "Familia1", Id = 1, Name = "Nombre1", NumHijos = 1 });
+            return View(listaPersonaje) ;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Personaje personaje)
+        {
+            
+            
+            personaje.Id = listaPersonaje.Count + 1;
+            
+            if (ModelState.IsValid)
+            {
+                listaPersonaje.Add(personaje);
+                return View("Index",listaPersonaje);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Personaje.cs
@@ -0,0 +1,10 @@
+﻿namespace AUT02_02_GaldonMario_Listas.Models
+{
+    public class Personaje
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumHijos { get; set; }
+        public string Familia { get; set; }
+    }
+}
