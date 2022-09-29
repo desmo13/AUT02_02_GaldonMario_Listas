057add325aafe7f624d6b977ad1115d3e4b9f043
desmo13 <mariodesmo13@gmail.com>
2022-09-29 16:52:06 +0100

    creado generador de id

--- Controllers/PersonajeController.cs
+++ Controllers/PersonajeController.cs
@@ -15,7 +15,41 @@ namespace AUT02_02_GaldonMario_Listas.Controllers
                 new Personaje() { Familia = "Familia3", Id = 2, Nombre = "Manolo", NumeroHijos = 10 }
             };
           
-        
+        private int GeneradorId(List<Personaje> lista)
+        {
+            int indice =0;
+            int posibleId=0;
+
+            bool final=false;
+            while (!final)
+            {
+                if (indice < lista.Count)
+                {
+                    if (lista[indice].Id != posibleId)
+                    {
+                        final = true;
+                    }
+                    else
+                    {
+                        indice++;
+                        posibleId++;
+                    }
+
+                }
+                else
+                {
+
+                    final = true;
+                }
+                
+                
+                    
+                
+               
+
+            }
+            return posibleId;
+        }
 
         public IActionResult Index()
         {
@@ -32,7 +66,7 @@ namespace AUT02_02_GaldonMario_Listas.Controllers
         {
             
             
-            personaje.Id = listaPersonaje.Count + 1;
+            personaje.Id = GeneradorId(listaPersonaje);
             
             if (ModelState.IsValid)
             {
@@ -48,7 +82,7 @@ namespace AUT02_02_GaldonMario_Listas.Controllers
         /*Funcion que es llamada cuando se apreta el boton de borrar de la lista 
          * Cada boton que se genera en el formulario se genera con un valor que envia diferente que es el ID de la persona 
          * Luego lo recogemos en esta funcion y en un bucle foreach vamos comparando si encuentra un id en la lista igual al enviado lo borra y sale del bucle con un break
-         * PD:que pestano no vea que e metido un break en un blucle for si no me mata
+         * 
          */
         public IActionResult Delete(int Id)
         {
